50184421ef86e827b5a9d51394a7ede9cd9ea2d5
billxue <xueeinstein@gmail.com>
2015-03-08 22:04:44 +0800

    debug restore

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 curr=$PWD;
-git pull origin master;
+#git pull origin master;
 
 if [ ! -d "tmp" ]; then
 	mkdir tmp;
@@ -10,8 +10,8 @@ fi
 
 function  backup() {
 	ls -a ~ | tail -n +3 > tmp/old;
-	ls -a . | tail -n +3 > tmp/new;
-	commFiles=$(comm -12 tmp/old tmp/new | grep -v -w 'tmp');
+	ls -a . | tail -n +3 | grep -v -w 'tmp' > tmp/new;
+	commFiles=$(comm -12 tmp/old tmp/new);
 	pushd ~;
 	echo $commFiles | xargs tar -zcvf "$curr/tmp/backup.tar.gz";
 	echo "$curr/tmp/backup.tar.gz generated";
@@ -28,11 +28,19 @@ function doIt() {
 function restore() {
 	pushd tmp/;
 	if [ -e 'backup.tar.gz' ]; then
-		tar -zxvf 'backup.tar.gz' backup;
+		rm -rf backup/;
+		mkdir backup;
+		tar -zxvf 'backup.tar.gz' -C backup;
 		ls -a backup/ | tail -n +3 > backup.data;
 		pushd backup/;
 		rsync -avh --no-perms . ~;
 		source ~/.bash_profile;
+		popd;
+		extraFiles=$(comm -13 backup.data new)
+		cd ~;
+		rm -i -r $extraFiles;
+		# mark! to do: delete extra files
+		exit 0;
 	else
 		echo "Cannot find backup file.";
 		exit 1;
@@ -41,6 +49,7 @@ function restore() {
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
 elif [ "$1" == "--restore" -o "$1" == "-r" ]; then
+	echo "restore...";
 	restore;
 else
 	backup;
