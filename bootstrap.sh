e6bbbfe1d02267d7931809bb79c5a58acd117f8f
xueeinstein <xueeinstein@gmail.com>
2016-04-14 00:11:04 +0800

    sync with home folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,8 +22,8 @@ function doIt() {
 	rsync --exclude ".git/" --exclude ".gitattributes" --exclude ".gitignore"  --exclude ".gitmodules" \
 		--exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "brew.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "tmp/" \
-		--exclude "Session.vim" --exclude ".tmux/" --exclude ".bashrc" \
-		-avnch --no-perms . ~;
+		--exclude "Session.vim" --exclude ".bashrc" \
+		-avch --no-perms . ~;
 	source ~/.bash_profile;
 	tmux source-file ~/.tmux.conf;
 }
